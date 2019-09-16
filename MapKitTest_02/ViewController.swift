08e77e76314298890c2b9e169942a113f9486ce0
dit03 <dit@imac-4.local>
2019-09-16 16:06:00 +0900

    New

--- MapKitTest_02/ViewController.swift
+++ MapKitTest_02/ViewController.swift
@@ -1,20 +1,52 @@
-//
-//  ViewController.swift
-//  MapKitTest_02
-//
-//  Created by dit03 on 2019. 9. 16..
-//  Copyright © 2019년 201730529. All rights reserved.
-//
-
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+
+    @IBOutlet weak var hybridBtn: UIBarButtonItem!
+    @IBOutlet weak var standardBtn: UIBarButtonItem!
+    @IBOutlet weak var satelliteBtn: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let location = CLLocationCoordinate2D(latitude: 35.165841, longitude: 129.072530)
+        let region = MKCoordinateRegion(center: location, latitudinalMeters: 300, longitudinalMeters: 300)
+        
+        let marker = MKPointAnnotation()
+        
+        marker.coordinate = location
+        marker.title = "동의과학대학교"
+        marker.subtitle = "DIT"
+        
+        mapView.addAnnotation(marker)
+        
+        mapView.mapType = MKMapType.hybrid
+        setEnables(hybrid: false, standard: true, satellite: true)
+        
+        mapView.setRegion(region, animated: true)
     }
-
-
+    
+    @IBAction func changeHybrid(_ sender: Any) {
+        mapView.mapType = MKMapType.hybrid
+        setEnables(hybrid: false, standard: true, satellite: true)
+    }
+    
+    @IBAction func changeStandard(_ sender: Any) {
+        mapView.mapType = MKMapType.standard
+        setEnables(hybrid: true, standard: false, satellite: true)
+    }
+    
+    @IBAction func changeSatellite(_ sender: Any) {
+        mapView.mapType = MKMapType.satellite
+        setEnables(hybrid: true, standard: true, satellite: false)
+    }
+    
+    func setEnables(hybrid: Bool, standard : Bool, satellite : Bool) {
+        hybridBtn.isEnabled = hybrid
+        standardBtn.isEnabled = standard
+        satelliteBtn.isEnabled = satellite
+    }
 }
-
